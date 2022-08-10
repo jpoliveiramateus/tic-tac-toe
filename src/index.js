c1618d583c3646a367329572c468f8df9dd58908
portuga <joaopedromateus6@gmail.com>
2022-08-09 21:19:55 -0300

    test: coverage +95%

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
